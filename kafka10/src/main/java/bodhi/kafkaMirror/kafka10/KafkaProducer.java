e41ff736e19cdf9220262363c3b0982f47789dfd
cchanley2003 <cchanley2003@yahoo.com>
2018-06-18 17:38:57 -0600

    Initial cut of kafka mirror

--- kafka10/src/main/java/bodhi/kafkaMirror/kafka10/KafkaProducer.java
+++ kafka10/src/main/java/bodhi/kafkaMirror/kafka10/KafkaProducer.java
@@ -3,4 +3,14 @@ package bodhi.kafkaMirror.kafka10;
 import bodhi.kafkaMirror.common.KafkaProducerInterface;
 
 public class KafkaProducer implements KafkaProducerInterface {
+
+    @Override
+    public void initConnection(String bootstrapServer) {
+
+    }
+
+    @Override
+    public void sendData(String data, String topic) {
+
+    }
 }
